c6c89cac40b98427f51eb0c02de80adcc70aa062
atal98 <80337520+atal98@users.noreply.github.com>
2024-05-22 16:51:34 +0530

    created new test on local

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -3,15 +3,12 @@ import "./navbar.scss";
 import Date from "../date/DateDropDown";
 
 // { title, onYearChange, onQuarterChange } -- comment for now
-const NavBar = ({ title }) => {
+const NavBar = ({ title, onYearChange, onQuarterChange }) => {
   return (
     <div className="navbar">
       <div className="wrapper">
         <span className="nav-title">{title}</span>
-        <Date
-        // onYearChange={onYearChange}
-        // onQuarterChange={onQuarterChange}
-        />
+        <Date onYearChange={onYearChange} onQuarterChange={onQuarterChange} />
       </div>
     </div>
   );
