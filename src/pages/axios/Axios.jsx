a0e68983655d0ad64fcc23a8f6f70b41484fe79a
atal98 <80337520+atal98@users.noreply.github.com>
2024-05-22 17:50:26 +0530

    update axios url

--- src/pages/axios/Axios.jsx
+++ src/pages/axios/Axios.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 // local_url = "http://127.0.0.1:8000/";
-const deployment_url = "https://admin-dashboard-be-2qkj.onrender.com/";
+const baseURL = "https://admin-dashboard-be-2qkj.onrender.com/";
 // const baseURL = "http://127.0.0.1:8000/";
-const baseURL = import.meta.env.VITE_API_URL
-  ? import.meta.env.VITE_API_URL
-  : deployment_url;
+// const baseURL = import.meta.env.VITE_API_URL
+//   ? import.meta.env.VITE_API_URL
+//   : deployment_url;
 const AxiosInstance = axios.create({
   baseURL: baseURL,
   timeout: 5000,
